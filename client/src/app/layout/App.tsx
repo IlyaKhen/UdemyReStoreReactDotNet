5fca39f1e895db4d4cbd2dbde916fbd9a1fe5be1
ilya.khenkin <khenkhan451@gmail.com>
2021-11-21 17:33:39 +0200

    updating project file structure

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
+import { Product } from "../models/product";
 
 function App() {
-  const [products, setProducts] = useState([
-    {name: 'product1', price: 100.00},
-    {name: 'product2', price: 200.00}
-  ]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:5000/api/products')
@@ -13,16 +11,23 @@ function App() {
   }, []) //effect callback and second param is dependency empty array (with dependency this method will be called only once)
 
   function addProduct() {
-    setProducts(prevState => [...products, 
-      {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}]);
+    setProducts(prevState => [...prevState, 
+      { 
+        id: prevState.length + 101,
+        name: 'product' + (prevState.length + 1), 
+        price: (prevState.length * 100) + 100,
+        brand: 'some brand',
+        description: 'some description',
+        pictureUrl: 'http://pic'
+      }]);
   }
 
   return (
     <div>
       <h1>Re-Store</h1>
       <ul>
-        {products.map( (item, index) => (
-          <li key={index}>{item.name} - {item.price}</li> 
+        {products.map( product => (
+          <li key={product.id}>{product.name} - {product.price}</li> 
         ))}
       </ul>
       <button onClick={addProduct}>Add Product</button>

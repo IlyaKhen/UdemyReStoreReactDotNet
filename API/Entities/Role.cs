b0cbf8635debc4e15e60fc471adf29ccc5510d54
ilya.khenkin <khenkhan451@gmail.com>
2021-12-08 18:36:56 +0200

    changing migrations, creating order functionality

--- API/Entities/Role.cs
+++ API/Entities/Role.cs
@@ -6,8 +6,8 @@ using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities
 {
-    public class User : IdentityUser<int> // int - override a string as PK that default for identity
+    public class Role: IdentityRole<int>
     {
-        public UserAddress Address { get; set; }
+        
     }
 }

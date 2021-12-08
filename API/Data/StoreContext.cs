36fe1a568e76d98da0c3582a84fe005bb8eeb20b
ilya.khenkin <khenkhan451@gmail.com>
2021-12-08 14:53:50 +0200

    creating orders table & classes

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Entities;
+using API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -13,19 +14,19 @@ namespace API.Data
     {
         public StoreContext(DbContextOptions options) : base(options)
         {
-
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder) //method from IdentityDbContext
         {
             base.OnModelCreating(builder);
 
             builder.Entity<IdentityRole>().HasData(
-                new IdentityRole{Name = "Member", NormalizedName = "MEMBER"},
-                new IdentityRole{Name = "Admin", NormalizedName = "ADMIN"}
+                new IdentityRole { Name = "Member", NormalizedName = "MEMBER" },
+                new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" }
             );
         }
 

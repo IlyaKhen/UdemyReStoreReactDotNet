2381528378859fc92f5e6357cc3c1f61602bb207
ilya.khenkin <khenkhan451@gmail.com>
2021-12-12 15:22:12 +0200

    order details logic

---
+++ client/src/features/orders/OrderDetailed.tsx
@@ -0,0 +1,35 @@
+import { Grid, Button, Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { BasketItem } from "../../app/models/basket";
+import { Order } from "../../app/models/order";
+import BasketSummary from "../basket/BasketSummary";
+import BasketTable from "../basket/BasketTable";
+
+interface Props {
+    order: Order;
+    setSelectedOrder: (id: number) => void;
+}
+
+export default function OrderDetailed({ order, setSelectedOrder }: Props) {
+    const subtotal = order.orderItems.reduce((sum, item) => sum + (item.quantity * item.price), 0) ?? 0;
+    return (
+        <>
+            <Box display='flex' justifyContent='space-between'>
+                <Typography sx={{ p: 2 }} gutterBottom variant='h4'>Order# {order.id} - {order.orderStatus}</Typography>
+                <Button onClick={() => setSelectedOrder(0)} sx={{ m: 2 }} size='large' variant='contained'>Back to orders</Button>
+            </Box>
+            <BasketTable items={order.orderItems as BasketItem[]} isBasket={false} />
+            <Grid container sx={{ pt: 2 }}>
+                <Grid item xs={6}>
+{/*                     <Typography variant="h6" gutterBottom>
+                        Shipping address
+                    </Typography> */}
+                </Grid>
+                <Grid item xs={6} sx={{ pl: 1 }}>
+                    <BasketSummary subtotal={subtotal} />
+                </Grid>
+            </Grid>
+
+        </>
+    )
+}
